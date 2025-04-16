c2c00f2dc70962bb1e9dd83dec6595bb8f31a0e1
dashkf07 <lemuzme1@gmail.com>
2025-04-16 21:47:18 +0300

    change vars in login form

--- src/features/auth/login/ui/login-form.tsx
+++ src/features/auth/login/ui/login-form.tsx
@@ -17,7 +17,7 @@ export const LoginForm = () => {
 
     try {
       const data = await login(username, password);
-      setUser(data["access-token"], data["refresh-token"]);
+      setUser(data["access_token"], data["refresh_token"]);
       // navigate("/dashboard");
     } catch (err) {
       setError("Неверные данные для входа");
